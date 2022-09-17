cd1daebff799a24f781fef52ba5ecf7d804e7c4f
Edmilson Santana <42233304+edmilsonss@users.noreply.github.com>
2022-09-16 23:17:54 -0300

    token automatic update refresh

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import axios, { Axios, AxiosRequestConfig, AxiosResponse } from 'axios';
 import axiosRetry from 'axios-retry';
 
+import { Issuer } from 'openid-client';
 import querystring from 'query-string';
 import camelize from 'camelize';
 
@@ -51,17 +52,21 @@ export interface TokenResponse {
 }
 
 interface TokenResponseRaw {
-  access_token: string;
+  access_token: string | undefined;
   expires_in: string;
-  refresh_expires_in: number;
-  refresh_token: string;
-  token_type: string;
-  not_before_policy: number;
-  session_state: string;
-  scope: string;
+  refresh_expires_in?: number;
+  refresh_token?: string;
+  token_type?: string;
+  not_before_policy?: number;
+  session_state?: string;
+  scope?: string;
 }
 
 export default class KeycloakAPI {
+
+  tokenUpdateSet: boolean = false;
+
+  currentTokenInfo: TokenResponseRaw | undefined;
   config: ServerSettings;
   httpClient: Axios;
 
@@ -90,43 +95,83 @@ export default class KeycloakAPI {
   }
 
   async getToken(settings: ServerSettings): Promise<TokenResponse> {
-    // Construct URL
-    const baseUrl = settings.baseUrl;
-    const realmName = settings.realmName || 'master';
-    const url = `${baseUrl}/realms/${realmName}/protocol/openid-connect/token`;
 
-    // Prepare credentials for openid-connect token request
-    // ref: http://openid.net/specs/openid-connect-core-1_0.html#TokenEndpoint
-    const credentials = settings.credentials || ({} as any);
-    const payload = querystring.stringify({
-      username: credentials.username,
-      password: credentials.password,
-      grant_type: credentials.grantType,
-      client_id: credentials.clientId,
-      totp: credentials.totp,
-      ...(credentials.offlineToken ? { scope: 'offline_access' } : {}),
-      ...(credentials.refreshToken
-        ? {
-          refresh_token: credentials.refreshToken,
-          client_secret: credentials.clientSecret,
-        }
-        : {}),
-    });
+    if (this.currentTokenInfo?.access_token) {
+      // Construct URL
+      const baseUrl = settings.baseUrl;
+      const realmName = settings.realmName || 'master';
+      const url = `${baseUrl}/realms/${realmName}/protocol/openid-connect/token`;
 
-    const config: AxiosRequestConfig = {
-      ...settings.requestConfig,
-    };
+      // Prepare credentials for openid-connect token request
+      // ref: http://openid.net/specs/openid-connect-core-1_0.html#TokenEndpoint
+      const credentials = settings.credentials || ({} as any);
+      const payload = querystring.stringify({
+        username: credentials.username,
+        password: credentials.password,
+        grant_type: credentials.grantType,
+        client_id: credentials.clientId,
+        totp: credentials.totp,
+        ...(credentials.offlineToken ? { scope: 'offline_access' } : {}),
+        ...(credentials.refreshToken
+          ? {
+            refresh_token: credentials.refreshToken,
+            client_secret: credentials.clientSecret,
+          }
+          : {}),
+      });
 
-    if (credentials.clientSecret) {
-      config.auth = {
-        username: credentials.clientId,
-        password: credentials.clientSecret,
+      const config: AxiosRequestConfig = {
+        ...settings.requestConfig,
       };
-    }
 
-    const { data } = await axios.post<any, AxiosResponse<TokenResponseRaw>>(url, payload, config).catch((err) => {
-      return err.response?.data ? err.response.data : err.response ? err.response : err;
-    });
-    return camelize(data);
+      if (credentials.clientSecret) {
+        config.auth = {
+          username: credentials.clientId,
+          password: credentials.clientSecret,
+        };
+      }
+
+      const { data } = await axios.post<any, AxiosResponse<TokenResponseRaw>>(url, payload, config).catch((err) => {
+        return err.response?.data ? err.response.data : err.response ? err.response : err;
+      });
+
+      this.currentTokenInfo = data;
+
+      if (!this.tokenUpdateSet) {
+
+        const keycloakIssuer = await Issuer.discover(`${baseUrl}/realms/${realmName}`);
+
+        const client = new keycloakIssuer.Client({
+          client_id: credentials.clientId,
+          token_endpoint_auth_method: 'none', // to send only client_id in the header
+        });
+
+        // Use the grant type 'password'
+        let tokenSet = await client.grant({
+          grant_type: 'password',
+          username: credentials.username,
+          password: credentials.password,
+        });
+
+        // Periodically using refresh_token grant flow to get new access token here
+        setInterval(async () => {
+          const refreshToken = tokenSet.refresh_token;
+          if (refreshToken) {
+            tokenSet = await client.refresh(refreshToken);
+            this.currentTokenInfo = {
+              access_token: tokenSet.access_token,
+              expires_in: tokenSet.expires_in ? String(tokenSet.expires_in) : '',
+              refresh_token: tokenSet.refresh_token,
+              scope: tokenSet.scope,
+              token_type: tokenSet.token_type,
+              session_state: tokenSet.session_state
+            };
+          }
+          console.log('tokenSet refreshed');
+          this.tokenUpdateSet = true;
+        }, 58 * 1000); // 58 seconds
+      }
+    }
+    return camelize(this.currentTokenInfo);
   }
 }
